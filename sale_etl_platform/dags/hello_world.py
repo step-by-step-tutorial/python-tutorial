3cbd5111908db064804a7d6d2135420898c07ec4
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2026-07-08 12:42:42 +0200

    new update

--- sale_etl_platform/dags/hello_world.py
+++ sale_etl_platform/dags/hello_world.py
@@ -1,9 +1,7 @@
-import datetime
-
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
 
 with DAG(dag_id="hello_world") as dag:
-    task1 = EmptyOperator(task_id="task")
+    task = EmptyOperator(task_id="task")
 
-    task1
+    task
--- sale_etl_platform/dags/sales_etl_pipeline.py
+++ sale_etl_platform/dags/sales_etl_pipeline.py
@@ -1,7 +1,4 @@
-from datetime import datetime
-
 from airflow.sdk import DAG
-from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.standard.operators.python import PythonOperator
 from sales_etl_pipeline_service import SalesEtlPipelineService
 
@@ -12,7 +9,6 @@ def sales_etl_pipeline() -> None:
 
 
 with DAG(dag_id="sales_etl_pipeline") as dag:
-    task1 = EmptyOperator(task_id="task_1")
-    task2 = PythonOperator(task_id="sales_etl_pipeline", python_callable=sales_etl_pipeline)
+    task = PythonOperator(task_id="sales_etl_pipeline", python_callable=sales_etl_pipeline)
 
-    task1 >> task2
+    task
