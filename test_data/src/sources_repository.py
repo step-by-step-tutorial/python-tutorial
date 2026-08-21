c1b6ab925ec4b635212ddfc2f851e8f7de372313
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2026-08-21 13:14:08 +0200

    new update

--- test_data/src/sources_repository.py
+++ test_data/src/sources_repository.py
@@ -8,7 +8,7 @@ class SourceRepository:
 
     def __init__(self) -> None:
         self.file_map: dict[str, tuple[str, ...]] = {}
-        self._mappings: dict[tuple[str, str, str], Mapping[str, str]] = {}
+        self.csv_map: dict[tuple[str, str, str], Mapping[str, str]] = {}
 
     def read_text_file(self, path: str) -> tuple[str, ...]:
         if path not in self.file_map:
@@ -18,6 +18,6 @@ class SourceRepository:
 
     def read_csv_file(self, path: str, key_column: str, value_column: str) -> Mapping[str, str]:
         key = (path, key_column, value_column)
-        if key not in self._mappings:
-            self._mappings[key] = extract_map_from_csv(path, key_column, value_column)
-        return self._mappings[key]
+        if key not in self.csv_map:
+            self.csv_map[key] = extract_map_from_csv(path, key_column, value_column)
+        return self.csv_map[key]
