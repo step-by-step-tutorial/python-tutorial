c9b6dd1aa544133f144ebce781dc1f6c481ec8b2
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2026-08-26 01:22:50 +0200

    new update

--- data_platform/src/data_platform/config/data_simulator_settings.py
+++ data_platform/src/data_platform/config/data_simulator_settings.py
@@ -3,14 +3,14 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class TestDataSettings:
+class DataSimulatorSettings:
     api_url: str
 
     def download_url(self, dataset_name: str) -> str:
         return f"{self.api_url.rstrip('/')}/datasets/{dataset_name.lower()}.json/download?format=json"
 
 
-test_data = TestDataSettings(
+test_data = DataSimulatorSettings(
     api_url=os.getenv("TEST_DATA_API_URL", "http://localhost:8080"),
 )
 
